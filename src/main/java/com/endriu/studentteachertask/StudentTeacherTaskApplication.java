c7c32e66529c9ac3289c365538bbd66d1c1fdc81
AndrzejSasak <andrzej.sasak01@gmail.com>
2022-11-20 19:27:22 +0100

    Initial commit

---
+++ src/main/java/com/endriu/studentteachertask/StudentTeacherTaskApplication.java
@@ -0,0 +1,13 @@
+package com.endriu.studentteachertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTeacherTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTeacherTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endriu/studentteachertask/StudentTeacherTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endriu.studentteachertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTeacherTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
